d981bc7efeb28323230d7b3f1af70c8d1911156b
Jianiqng Ma <jianqing997@gmail.com>
2023-10-29 15:58:27 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// step 1: set up the app framework: using express
+const express = require('express');
+const app = express();
+
+// step 2: set up the view engine: ejs
+//
+const mongoose = require('mongoose');
